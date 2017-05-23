af6bfcaaaf5fe02adbb4b916a9e008ae53c60d28
XOR-IND\Jagtap_R <roshan.jagtap@xoriant.com>
2017-05-23 18:19:59 +0530

    NA-1190 EngApp-SplashScreen Code commenting

--- MercuryEngApp/MercuryEngApp/Views/SplashScreen.xaml.cs
+++ MercuryEngApp/MercuryEngApp/Views/SplashScreen.xaml.cs
@@ -21,74 +21,10 @@ namespace MercuryEngApp.Views
 /// <seealso cref="Windows.UI.Xaml.Controls.Page" />
     public partial class SplashScreen : Window
     {
-         #region Constatnts
-
-        /// <summary>
-        /// The wait to display splash
-        /// </summary>
-        private const int WAIT_TO_DISPLAY_SPLASH = 4500;
-        /// <summary>
-        /// The na tech identifier
-        /// </summary>
-        private const int NA_TECH_ID = 2;
-        /// <summary>
-        /// The database folder name
-        /// </summary>
-        private const string DATABASE_FOLDER_NAME = "Database";
-        /// <summary>
-        /// The database file name
-        /// </summary>
-        private const string DATABASE_FILE_NAME = "Mercury.sqlite";
-
-        private const int VALUE_8 = 8;
-        private const int VALUE_1 = 1;
-
-        #endregion
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SpashScreen"/> class.
-        /// </summary>
         public SplashScreen()
         {
-            this.InitializeComponent();
-            Loaded+=SplashScreenLoaded;
+            this.InitializeComponent();           
         }
 
-        /// <summary>
-        /// Spashes the screen loaded.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
-        private async void SplashScreenLoaded(object sender, RoutedEventArgs e)
-        {
-            //await Task.Delay(9000);  
-            //this.Close();
-        }
-
-        ///// <summary>
-        ///// Decides the home page.
-        ///// </summary>
-        //private void DecideHomePage()
-        //{
-        //    MainViewModel loginViewModel = new MainViewModel();
-        //    MainWindow login = loginViewModel.GetMainWindowByUserID(NA_TECH_ID);
-        //    if (login==null)
-        //    {
-               
-        //    }
-        //    else
-        //    {
-                
-        //    }
-        //}
-       
-        /// <summary>
-        /// Copy the database
-        /// </summary>
-        /// <returns>Task</returns>
-     
-        /// <summary>
-        /// Load the exam vessels for each exam procedure
-        /// </summary>
-       
     }
 }
