1f4645ad9f4fc9b386fa87757a510f708fc245cf
unknown <gupta_am@Salil-BK.India.XoriantCorp.com>
2017-05-23 17:40:42 +0530

    Changes for logging using log4net

--- MercuryEngApp/MercuryEngApp/Views/PacketControl.xaml.cs
+++ MercuryEngApp/MercuryEngApp/Views/PacketControl.xaml.cs
@@ -20,7 +20,7 @@ using System.Windows.Controls.Primitives;
 using UsbTcdLibrary;
 using MercuryEngApp.Common;
 using UsbTcdLibrary.PacketFormats;
-
+using log4net;
 
 namespace MercuryEngApp
 {
@@ -29,7 +29,8 @@ namespace MercuryEngApp
     /// </summary>
     public partial class PacketControl : UserControl
     {
-        public bool IsManualClickSelect = true;
+        public static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public PacketControl()
         {
             InitializeComponent();
@@ -39,104 +40,112 @@ namespace MercuryEngApp
 
         private void LoadTreeView()
         {
-            //Root Item
-            ItemsMenu PacketRoot = GetMenuItem("Packet", 0, "packet");
-            PacketRoot.IsExpanded = true;
-            // 1th Element 
-            ItemsMenu ChildHeader = GetMenuItem("Header", ServiceHeader.sync, "header");
-            ChildHeader.Items.Add(GetMenuItem("Sync", ServiceHeader.sync, "long"));
-            ChildHeader.Items.Add(GetMenuItem("SystemID", ServiceHeader.systemId, "byte"));
-            ChildHeader.Items.Add(GetMenuItem("DataSource", ServiceHeader.dataSource, "byte"));
-            ChildHeader.Items.Add(GetMenuItem("MessageType", ServiceHeader.messageType, "byte"));
-            ChildHeader.Items.Add(GetMenuItem("MessageSubType", ServiceHeader.messageSubType, "byte"));
-            ChildHeader.Items.Add(GetMenuItem("DataLength", ServiceHeader.dataLength, "ushort"));
-            ChildHeader.Items.Add(GetMenuItem("Sequence", ServiceHeader.sequence, "ushort"));
-            PacketRoot.Items.Add(ChildHeader);
+            try
+            {
+                //Root Item
+                ItemsMenu PacketRoot = GetMenuItem("Packet", 0, "packet");
+                PacketRoot.IsExpanded = true;
+                // 1th Element 
+                ItemsMenu ChildHeader = GetMenuItem("Header", ServiceHeader.sync, "header");
+                ChildHeader.Items.Add(GetMenuItem("Sync", ServiceHeader.sync, "long"));
+                ChildHeader.Items.Add(GetMenuItem("SystemID", ServiceHeader.systemId, "byte"));
+                ChildHeader.Items.Add(GetMenuItem("DataSource", ServiceHeader.dataSource, "byte"));
+                ChildHeader.Items.Add(GetMenuItem("MessageType", ServiceHeader.messageType, "byte"));
+                ChildHeader.Items.Add(GetMenuItem("MessageSubType", ServiceHeader.messageSubType, "byte"));
+                ChildHeader.Items.Add(GetMenuItem("DataLength", ServiceHeader.dataLength, "ushort"));
+                ChildHeader.Items.Add(GetMenuItem("Sequence", ServiceHeader.sequence, "ushort"));
+                PacketRoot.Items.Add(ChildHeader);
 
-            // 2nd Element
-            PacketRoot.Items.Add(GetMenuItem("Reserved", Header.Reserved, "ushort"));
+                // 2nd Element
+                PacketRoot.Items.Add(GetMenuItem("Reserved", Header.Reserved, "ushort"));
 
-            //3rd Element
-            PacketRoot.Items.Add(GetMenuItem("DataFormatREV", Header.DataFormatREV, "ushort"));
+                //3rd Element
+                PacketRoot.Items.Add(GetMenuItem("DataFormatREV", Header.DataFormatREV, "ushort"));
 
-            // 4th Element 
-            ItemsMenu ChildParameter = GetMenuItem("Parameter", Parameter.LeftTimeStamp, "parameter");
-            ChildParameter.Items.Add(GetMenuItem("TimestampL", Parameter.LeftTimeStamp, "uint"));
-            ChildParameter.Items.Add(GetMenuItem("TimestampH", Parameter.RightTimeStamp, "uint"));
-            ChildParameter.Items.Add(GetMenuItem("EventFlags", Parameter.EventFlags, "ushort"));
-            ChildParameter.Items.Add(GetMenuItem("OperatingState", Parameter.OperatingState, "byte"));
-            ChildParameter.Items.Add(GetMenuItem("AcousticPower", Parameter.AcousingPower, "byte"));
-            ChildParameter.Items.Add(GetMenuItem("SampleLength", Parameter.SampleLength, "byte"));
-            ChildParameter.Items.Add(GetMenuItem("UserDepth", Parameter.UserDepth, "byte"));
-            ChildParameter.Items.Add(GetMenuItem("PRF", Parameter.PRF, "ushort"));
-            ChildParameter.Items.Add(GetMenuItem("TIC", Parameter.TIC, "ushort"));
-            ChildParameter.Items.Add(GetMenuItem("RFU", Parameter.RFU, "ushort"));
-            PacketRoot.Items.Add(ChildParameter);
+                // 4th Element 
+                ItemsMenu ChildParameter = GetMenuItem("Parameter", Parameter.LeftTimeStamp, "parameter");
+                ChildParameter.Items.Add(GetMenuItem("TimestampL", Parameter.LeftTimeStamp, "uint"));
+                ChildParameter.Items.Add(GetMenuItem("TimestampH", Parameter.RightTimeStamp, "uint"));
+                ChildParameter.Items.Add(GetMenuItem("EventFlags", Parameter.EventFlags, "ushort"));
+                ChildParameter.Items.Add(GetMenuItem("OperatingState", Parameter.OperatingState, "byte"));
+                ChildParameter.Items.Add(GetMenuItem("AcousticPower", Parameter.AcousingPower, "byte"));
+                ChildParameter.Items.Add(GetMenuItem("SampleLength", Parameter.SampleLength, "byte"));
+                ChildParameter.Items.Add(GetMenuItem("UserDepth", Parameter.UserDepth, "byte"));
+                ChildParameter.Items.Add(GetMenuItem("PRF", Parameter.PRF, "ushort"));
+                ChildParameter.Items.Add(GetMenuItem("TIC", Parameter.TIC, "ushort"));
+                ChildParameter.Items.Add(GetMenuItem("RFU", Parameter.RFU, "ushort"));
+                PacketRoot.Items.Add(ChildParameter);
 
-            // 5th Element 
-            PacketRoot.Items.Add(GetMenuItem("EmbCount", Parameter.EmboliCount, "uint"));
+                // 5th Element 
+                PacketRoot.Items.Add(GetMenuItem("EmbCount", Parameter.EmboliCount, "uint"));
 
-            // 6th Element 
-            ItemsMenu ChildEnvelop = GetMenuItem("Envelop", Envelop.Depth, "envelop");
-            ChildEnvelop.Items.Add(GetMenuItem("Depth", Envelop.Depth, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("VelocityUnits", Envelop.VelocityUnit, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("ColIndexPos", Envelop.ColIndexPos, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("PosVelocity", Envelop.PosVelocity, "short"));
-            ChildEnvelop.Items.Add(GetMenuItem("PosPEAK", Envelop.PosPeak, "short"));
-            ChildEnvelop.Items.Add(GetMenuItem("PosMEAN", Envelop.PosMean, "short"));
-            ChildEnvelop.Items.Add(GetMenuItem("PosDIAS", Envelop.PosDias, "short"));
-            ChildEnvelop.Items.Add(GetMenuItem("PosPI", Envelop.PosPI, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("PosRI", Envelop.PosRI, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("ColIndexNeg", Envelop.ColIndexNeg, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("NegVelocity", Envelop.NegVelocity, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("NegPEAK", Envelop.NegPeak, "short"));
-            ChildEnvelop.Items.Add(GetMenuItem("NegMEAN", Envelop.NegMean, "short"));
-            ChildEnvelop.Items.Add(GetMenuItem("NegDIAS", Envelop.NegDias, "short"));
-            ChildEnvelop.Items.Add(GetMenuItem("NegPI", Envelop.NegPI, "ushort"));
-            ChildEnvelop.Items.Add(GetMenuItem("NegRI", Envelop.NegRI, "ushort"));
-            PacketRoot.Items.Add(ChildEnvelop);
+                // 6th Element 
+                ItemsMenu ChildEnvelop = GetMenuItem("Envelop", Envelop.Depth, "envelop");
+                ChildEnvelop.Items.Add(GetMenuItem("Depth", Envelop.Depth, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("VelocityUnits", Envelop.VelocityUnit, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("ColIndexPos", Envelop.ColIndexPos, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("PosVelocity", Envelop.PosVelocity, "short"));
+                ChildEnvelop.Items.Add(GetMenuItem("PosPEAK", Envelop.PosPeak, "short"));
+                ChildEnvelop.Items.Add(GetMenuItem("PosMEAN", Envelop.PosMean, "short"));
+                ChildEnvelop.Items.Add(GetMenuItem("PosDIAS", Envelop.PosDias, "short"));
+                ChildEnvelop.Items.Add(GetMenuItem("PosPI", Envelop.PosPI, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("PosRI", Envelop.PosRI, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("ColIndexNeg", Envelop.ColIndexNeg, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("NegVelocity", Envelop.NegVelocity, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("NegPEAK", Envelop.NegPeak, "short"));
+                ChildEnvelop.Items.Add(GetMenuItem("NegMEAN", Envelop.NegMean, "short"));
+                ChildEnvelop.Items.Add(GetMenuItem("NegDIAS", Envelop.NegDias, "short"));
+                ChildEnvelop.Items.Add(GetMenuItem("NegPI", Envelop.NegPI, "ushort"));
+                ChildEnvelop.Items.Add(GetMenuItem("NegRI", Envelop.NegRI, "ushort"));
+                PacketRoot.Items.Add(ChildEnvelop);
 
-            // 7th Element 
-            ItemsMenu ChildSpectrum = GetMenuItem("Spectrum", Spectrum.Depth, "spectrum");
-            ChildSpectrum.Items.Add(GetMenuItem("Depth", Spectrum.Depth, "ushort"));
-            ChildSpectrum.Items.Add(GetMenuItem("ClutterFilter", Spectrum.ClutterFilter, "ushort"));
-            ChildSpectrum.Items.Add(GetMenuItem("AutoGainOffset", Spectrum.AutoGainOffset, "short"));
-            ChildSpectrum.Items.Add(GetMenuItem("StartVelocity", Spectrum.StartVelocity, "short"));
-            ChildSpectrum.Items.Add(GetMenuItem("EndVelocity", Spectrum.EndVelocity, "short"));
-            ChildSpectrum.Items.Add(GetMenuItem("PointsPerColumn", Spectrum.PointsPerColumn, "ushort"));
-            ChildSpectrum.Items.Add(GetMenuItem("Points", Spectrum.Points, "points"));
-            PacketRoot.Items.Add(ChildSpectrum);
-            trvMenu.Items.Add(PacketRoot);
+                // 7th Element 
+                ItemsMenu ChildSpectrum = GetMenuItem("Spectrum", Spectrum.Depth, "spectrum");
+                ChildSpectrum.Items.Add(GetMenuItem("Depth", Spectrum.Depth, "ushort"));
+                ChildSpectrum.Items.Add(GetMenuItem("ClutterFilter", Spectrum.ClutterFilter, "ushort"));
+                ChildSpectrum.Items.Add(GetMenuItem("AutoGainOffset", Spectrum.AutoGainOffset, "short"));
+                ChildSpectrum.Items.Add(GetMenuItem("StartVelocity", Spectrum.StartVelocity, "short"));
+                ChildSpectrum.Items.Add(GetMenuItem("EndVelocity", Spectrum.EndVelocity, "short"));
+                ChildSpectrum.Items.Add(GetMenuItem("PointsPerColumn", Spectrum.PointsPerColumn, "ushort"));
+                ChildSpectrum.Items.Add(GetMenuItem("Points", Spectrum.Points, "points"));
+                PacketRoot.Items.Add(ChildSpectrum);
+                trvMenu.Items.Add(PacketRoot);
 
-            // 8th Element 
-            ItemsMenu ChildMmode = GetMenuItem("Mmode", MMode.AutoGainOffset, "Mmode");
-            ChildMmode.Items.Add(GetMenuItem("AutoGainOffset", MMode.AutoGainOffset, "short"));
-            ChildMmode.Items.Add(GetMenuItem("StartDepth", MMode.StartDepth, "ushort"));
-            ChildMmode.Items.Add(GetMenuItem("EndDepth", MMode.EndDepth, "ushort"));
-            ChildMmode.Items.Add(GetMenuItem("PointsPerColumn", MMode.PointsPerColumn, "ushort"));
-            ChildMmode.Items.Add(GetMenuItem("Power", MMode.Power, "power"));
-            ChildMmode.Items.Add(GetMenuItem("Velocity", MMode.Velocity, "velocity"));
-            PacketRoot.Items.Add(ChildMmode);
+                // 8th Element 
+                ItemsMenu ChildMmode = GetMenuItem("Mmode", MMode.AutoGainOffset, "Mmode");
+                ChildMmode.Items.Add(GetMenuItem("AutoGainOffset", MMode.AutoGainOffset, "short"));
+                ChildMmode.Items.Add(GetMenuItem("StartDepth", MMode.StartDepth, "ushort"));
+                ChildMmode.Items.Add(GetMenuItem("EndDepth", MMode.EndDepth, "ushort"));
+                ChildMmode.Items.Add(GetMenuItem("PointsPerColumn", MMode.PointsPerColumn, "ushort"));
+                ChildMmode.Items.Add(GetMenuItem("Power", MMode.Power, "power"));
+                ChildMmode.Items.Add(GetMenuItem("Velocity", MMode.Velocity, "velocity"));
+                PacketRoot.Items.Add(ChildMmode);
 
-            // 3rd Element 
-            ItemsMenu ChildP3 = GetMenuItem("Audio", Audio.Depth, "audio");
-            ChildP3.Items.Add(GetMenuItem("Depth", Audio.Depth, "ushort"));
-            ChildP3.Items.Add(GetMenuItem("RFU", Audio.RFU, "ushort"));
-            ChildP3.Items.Add(GetMenuItem("SampleRate", Audio.SampleRate, "ushort"));
-            ChildP3.Items.Add(GetMenuItem("MaxAmplitude", Audio.MaxAmplitude, "ushort"));
-            ChildP3.Items.Add(GetMenuItem("Toward", Audio.Toward, "toward"));
-            ChildP3.Items.Add(GetMenuItem("Away", Audio.Away, "away"));
-            PacketRoot.Items.Add(ChildP3);
+                // 3rd Element 
+                ItemsMenu ChildP3 = GetMenuItem("Audio", Audio.Depth, "audio");
+                ChildP3.Items.Add(GetMenuItem("Depth", Audio.Depth, "ushort"));
+                ChildP3.Items.Add(GetMenuItem("RFU", Audio.RFU, "ushort"));
+                ChildP3.Items.Add(GetMenuItem("SampleRate", Audio.SampleRate, "ushort"));
+                ChildP3.Items.Add(GetMenuItem("MaxAmplitude", Audio.MaxAmplitude, "ushort"));
+                ChildP3.Items.Add(GetMenuItem("Toward", Audio.Toward, "toward"));
+                ChildP3.Items.Add(GetMenuItem("Away", Audio.Away, "away"));
+                PacketRoot.Items.Add(ChildP3);
 
-            // 2nd Element
-            ItemsMenu ChildP2 = new ItemsMenu() { Title = "Archive" };
-            //Add to root
-            PacketRoot.Items.Add(ChildP2);
+                // 2nd Element
+                ItemsMenu ChildP2 = new ItemsMenu() { Title = "Archive" };
+                //Add to root
+                PacketRoot.Items.Add(ChildP2);
 
-            // 4th Element
-            PacketRoot.Items.Add(GetMenuItem("CheckSum", 1128, "int"));
-            trvMenu.SelectedItemChanged += TreeItem_Selected;
+                // 4th Element
+                PacketRoot.Items.Add(GetMenuItem("CheckSum", 1128, "int"));
+                trvMenu.SelectedItemChanged += TreeItem_Selected;
 
+                Log.Debug("TreeView Created");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message.ToString());
+            }
         }
 
         void TreeItem_Selected(object sender, RoutedEventArgs e)
