da5f2fb3dd8b2ef4ac87fc9286a24e516fe66447
XOR-IND\gade_m <mohan.gade@xoriant.com>
2017-06-01 17:40:32 +0530

    Added test for NAGraph

--- MercuryEngApp/MercuryEngApp.Test/PlottingLib/NAGraphTest.cs
+++ MercuryEngApp/MercuryEngApp.Test/PlottingLib/NAGraphTest.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PlottingLib;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 namespace MercuryEngApp.Test.PlottingLib
 {
-    class NAGraphTest
+    [TestClass]
+    public class NAGraphTest
     {
+        public NaGraph NaGraph { get; set; }
+
+        public NAGraphTest()
+        {
+            WriteableBitmap dummyBitmap = BitmapFactory.New(10, 10);
+            NaGraph = new NaGraph(new BitmapList {
+                LeftMmodeBitmap = dummyBitmap, 
+                LeftSpectrumBitmap = dummyBitmap,
+                RightMmodeBitmap = dummyBitmap,
+                RightSpectrumBitmap = dummyBitmap
+            });
+        }
+
+        [TestMethod]
+        public void InitializeTest_FourSecond()
+        {
+            SpectrumXScale scale = SpectrumXScale.FourSecond;
+            var gain = 10;
+            NaGraph.Initialize(scale,gain);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void InitializeTest_EightSecond()
+        {
+            SpectrumXScale scale = SpectrumXScale.EightSecond;
+            var gain = 10;
+            NaGraph.Initialize(scale, gain);
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void InitializeTest_TwelveSecond()
+        {
+            SpectrumXScale scale = SpectrumXScale.TwelveSecond;
+            var gain = 10;
+            NaGraph.Initialize(scale, gain);
+            Assert.IsTrue(true);
+        }
+
     }
 }
