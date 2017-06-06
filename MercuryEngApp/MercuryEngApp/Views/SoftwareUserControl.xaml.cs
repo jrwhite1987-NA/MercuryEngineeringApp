bffe9afbe41d2b1db076bbc5e29f5adc5611e78d
XOR-IND\Ahirrao_r <rajendra.ahirrao@xoriant.com>
2017-06-06 14:26:40 +0530

    add new Tab Software Update & create new page Software User Control, changes InfoTab

---
+++ MercuryEngApp/MercuryEngApp/Views/SoftwareUserControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MercuryEngApp.Views
+{
+    /// <summary>
+    /// Interaction logic for SoftwareUserControl.xaml
+    /// </summary>
+    public partial class SoftwareUserControl : UserControl
+    {
+        public SoftwareUserControl()
+        {
+            InitializeComponent();
+        }
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                FileNameTextBox.Text = filename;
+
+                Paragraph paragraph = new Paragraph();
+                paragraph.Inlines.Add(System.IO.File.ReadAllText(filename));
+                FlowDocument document = new FlowDocument(paragraph);
+            }
+        }
+    }
+}
