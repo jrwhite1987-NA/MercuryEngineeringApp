70d8d9c98134193f788e1fa9487f07e40bff4615
XOR-IND\shinde_am <Amar.Shinde@Xoriant.com>
2017-06-16 10:25:19 +0530

    Unit Tests-SpectrogramSettingTest

---
+++ MercuryEngApp/MercuryEngApp.Test/MercuryEngApp/Common/SpectrogramSettingTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MercuryEngApp.Common;
+using System.Collections.Generic;
+using Core.Constants;
+
+namespace MercuryEngApp.Test.MercuryEngApp.Common
+{
+    [TestClass]
+    public class SpectrogramSettingTest
+    {
+        [TestMethod]
+        public void GetSpectrogramSettingTest1()
+        {
+            SpectrogramSetting spectrogramSetting =
+                SpectrogramSetting.GetSpectrogramSetting(Constants.VALUE_23, true);
+
+            Assert.IsTrue((spectrogramSetting != null));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),
+            "Value of depth can not be zero.")]
+        public void GetSpectrogramSettingTest2()
+        {
+            SpectrogramSetting spectrogramSetting =
+                SpectrogramSetting.GetSpectrogramSetting(0, false);
+        }
+
+        [TestMethod]
+        public void GetNearestVelocityRangeTest()
+        {
+            Assert.IsTrue(true);
+            //SpectrogramSetting.GetNearestVelocityRange(10)
+        }
+    }
+}
